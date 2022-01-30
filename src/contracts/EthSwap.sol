43cbe987e8acd623a97784dbd86b85d0e12dc559
lucas <lcs.carvajal@gmail.com>
2022-01-30 21:09:08 +0100

    add tests and code for and EthSwap contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,22 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        // calculate number of tokens to buy
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+    }
+
 }
+
+
